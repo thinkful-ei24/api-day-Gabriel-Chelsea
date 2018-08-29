019f23f5eb3631c8f2e759cfea86a87ab5405c28
Chelsea Kent <chelsea.kent@gmail.com>
2018-08-29 08:53:05 -0700

    added updateItem API method

--- scripts/api.js
+++ scripts/api.js
@@ -2,10 +2,13 @@
 
 const API = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/gabriel-chelsea';
+
+  //Get Items
   const getItems = function(callback) {
     $.getJSON(`${BASE_URL}/items`, callback);
   };
 
+  //Create Item
   const createItem = function(name, callback) {
     const newItem = JSON.stringify({ name: name });
 
@@ -18,8 +21,21 @@ const API = (function() {
     });
   };
 
+  //Update Item
+  const updateItem = function(id, updateData, callback) {
+    const updatedItem = JSON.stringify(updateData);
+    $.ajax({
+      url: `${BASE_URL}/items/${id}`,
+      method: 'PATCH',
+      contentType: 'application/json',
+      data: updatedItem,
+      success: callback
+    });
+  };
+
   return {
     getItems,
-    createItem
+    createItem,
+    updateItem
   };
 })();
