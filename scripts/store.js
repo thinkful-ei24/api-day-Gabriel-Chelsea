10e8b5883cad27962190cf0794b4d8a405733c66
Gabriel Morris <gabemorris@outlook.com>
2018-08-29 09:01:32 -0700

    Added find and update to store

--- scripts/store.js
+++ scripts/store.js
@@ -11,23 +11,13 @@ const store = (function() {
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
-
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
   };
 
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch (e) {
-      console.log('Cannot update name: ' + e.message);
-    }
+  const findAndUpdate = function(id, newData) {
+    const foundItem = this.items.find(item => item.id === id);
+    Object.assign(foundItem, newData);
   };
 
   const toggleCheckedFilter = function() {
@@ -45,10 +35,9 @@ const store = (function() {
 
     addItem,
     findById,
-    findAndToggleChecked,
     findAndDelete,
-    findAndUpdateName,
     toggleCheckedFilter,
-    setSearchTerm
+    setSearchTerm,
+    findAndUpdate
   };
 })();
